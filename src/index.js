55516b9ad9fac5e32aebf9661f45bbc06ec6ec26
Sam Chillcott <sam@samchillcott.com>
2020-08-29 15:43:31 +0100

    unfunctional way

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-let numbers = [1,2,3];
-numbers.map(number => number *2);
+import { compose, pipe } from "lodash/fp";
 
+let input = "     JavaScript    ";
+let output = "<div>" + input.trim() + "</div>";
 
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
 
+const result = wrapInDiv(toLowerCase(trim(input)));
