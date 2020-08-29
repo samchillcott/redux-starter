45323e7094a315db5f2d106e1467e601ffdc62c2
Sam Chillcott <sam@samchillcott.com>
2020-08-29 17:48:09 +0100

    store import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import { compose, pipe } from "lodash/fp";
+import store from "./store";
 
-let input = "     JavaScript    ";
-let output = "<div>" + input.trim() + "</div>";
-
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
-
-const transform = pipe(trim, toLowerCase, wrap("span"));
-console.log(transform(input));
-
-// const result = wrapInDiv(toLowerCase(trim(input)));
+console.log(store);
