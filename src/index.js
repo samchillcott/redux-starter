b74394e8f4eaffbb41cfe458f3ebc2a63b6dbc22
Sam Chillcott <sam@samchillcott.com>
2020-08-29 16:19:40 +0100

    curry function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let input = "     JavaScript    ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-const wrap = (type, str) => `<${type}>${str}</${type}>`;
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
 const transform = pipe(trim, toLowerCase, wrap);
