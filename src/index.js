424c2627ee8d68f49ce297d73563ecdef3e4a7dc
Sam Chillcott <sam@samchillcott.com>
2020-08-29 18:15:45 +0100

    subscribe

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import store from "./store";
 
+store.subscribe(() => {
+	console.log("Store changed!", store.getState());
+});
+
 store.dispatch({
 	type: "bugAdded",
 	payload: {
 		description: "bug1",
+	},
+});
+
+store.dispatch({
+	type: "bugRemoved",
+	payload: {
+		id: 1,
 	},
 });
 
