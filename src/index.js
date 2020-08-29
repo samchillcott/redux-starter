f1674f63baca63e8982fd2e1c64f9ee02a1f93d5
Sam Chillcott <sam@samchillcott.com>
2020-08-29 17:07:12 +0100

    redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const trim = (str) => str.trim();
 const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
-const transform = pipe(trim, toLowerCase, wrap);
+const transform = pipe(trim, toLowerCase, wrap("span"));
 console.log(transform(input));
 
 // const result = wrapInDiv(toLowerCase(trim(input)));
