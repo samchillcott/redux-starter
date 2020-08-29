a2eb7dd2036f95c5a55fea8016dac45e0a67618c
Sam Chillcott <sam@samchillcott.com>
2020-08-29 18:50:18 +0100

    resolved - actions

--- src/actions.js
+++ src/actions.js
@@ -3,15 +3,13 @@ import * as actions from "./actionTypes";
 export const bugAdded = (description) => ({
 	type: actions.BUG_ADDED,
 	payload: {
-		description: "bug1",
+		description,
 	},
 });
 
-// export function bugAdded(description) {
-// 	return {
-// 		type: actions.BUG_ADDED,
-// 		payload: {
-// 			description: "bug1",
-// 		},
-// 	};
-// }
+export const bugResolved = (id) => ({
+	type: actions.BUG_RESOLVED,
+	payload: {
+		id,
+	},
+});
